00102fdc8517e9754063bee1b94bfb7b9e6ec1ed
Artem Vasyatkin <dustler98@gmail.com>
2025-05-23 18:47:45 +0300

    Initial commit

---
+++ src/main/java/com/example/Bank_Star/BankStarApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bank_Star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Bank_Star/BankStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bank_Star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
